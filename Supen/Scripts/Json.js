3d2d7321a3bf24a549a7753fd42135eaea959fb1
Joakim Vestin <joavev131@studentmail.oru.com>
2015-03-24 11:15:17 +0100

    Json javascript

---
+++ Supen/Scripts/Json.js
@@ -0,0 +1,50 @@
+﻿
+
+function logIn() {
+    $('#LoginForm').submit(function() {
+
+        $('#LoginForm').validate();
+        if ($('#LoginForm').valid()) {
+            $.ajax({
+                url: "/Account/Login",
+                type: "POST",
+                data: $(this).serialize(),
+                success: function(result) {
+                    if (result == true) {
+                        window.location = "//";
+                    } else {//om inloggning misslyckas
+                        
+                    }
+                }
+            });
+        } else {//om validering av fält misslyckas
+
+        }
+        return false;
+    });
+}
+
+function Register() {
+    $('#RegistrationForm').submit(function () {
+
+        $('#RegistrationForm').validate();
+        if($('#RegistrationForm').valid()){
+            $.ajax({
+
+                url: "/Account/CreateUser",
+                type: "POST",
+                data: $(this).serialize(),
+                success: function(result) {
+                    if (result == true) {
+                        window.location = "";
+                    }
+                    else {
+                        
+                    }
+                }
+            });
+        }
+        return false;
+    });
+
+}
